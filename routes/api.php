3b099b07b0acaeae4e7bd55092cbbc9c09aaf581
nirjonroy <roynirjon18@gmail.com>
2025-01-29 21:17:56 +0600

    api for Slider

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\AuthController;
 use App\Http\Controllers\api\SiteInformationController;
+use App\Http\Controllers\api\SliderController;
 
 // Public API routes (No authentication required)
 Route::post('/login', [AuthController::class, 'login']); // ✅ Public route
@@ -13,4 +14,5 @@ Route::get('/user', [AuthController::class, 'user'])->middleware('auth:sanctum')
 // Secure API route with Sanctum authentication
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/site-information', [SiteInformationController::class, 'index']);   
+    Route::get('/slider', [SliderController::class, 'index']);
 });
